499838bcc1061a75b1c8007781b3c7372a33652a
Safa Marhaba <44847731+safaha243@users.noreply.github.com>
2022-04-21 14:12:50 +0300

    Update and rename class-icecat-integration-activator.php to class-wordpress-integration-activator.php

--- includes/class-wordpress-integration-activator.php
+++ includes/class-wordpress-integration-activator.php
@@ -6,8 +6,8 @@
  * @link       https://typhon.agency
  * @since      1.0.0
  *
- * @package    Icecat_Integration
- * @subpackage Icecat_Integration/includes
+ * @package    Wordpress_Integration
+ * @subpackage Wordpress_Integration/includes
  */
 
 /**
@@ -16,11 +16,11 @@
  * This class defines all code necessary to run during the plugin's activation.
  *
  * @since      1.0.0
- * @package    Icecat_Integration
- * @subpackage Icecat_Integration/includes
- * @author     Safa Marhaba <safa.marhaba@typhon.agency>
+ * @package    Wordpress_Integration
+ * @subpackage Wordpress_Integration/includes
+ * @author     Safa Marhaba <safamarhaba244@gmail.com>
  */
-class Icecat_Integration_Activator {
+class Wordpress_Integration_Activator {
 
 	/**
 	 * Short Description. (use period)
